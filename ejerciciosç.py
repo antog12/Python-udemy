c24b91e892cbb4a821cf9059659498ce9fc219ec
A.Angel <antog12@hotmail.com>
2022-07-17 10:03:25 +0200

    ejercicios varios

---
+++ ejerciciosç.py
@@ -0,0 +1,15 @@
+# ejercicio for exponente
+
+base=int(input("Ingrese la base: "))
+
+for exponente in range(1,11):
+    print(base,"^",exponente,"=",base**exponente)# vemos como el exponete va subiendo con el for.
+
+print("----------------------------------------------")
+
+# mostrar por pantalla el sigunente mensaje:
+
+mensaje="""La felicidad se puede encontrar hasta en los más oscuros momentos
+        ,si somos capaces de usar bien la luz
+        "- Albus Dumbledore"""
+print(mensaje)
