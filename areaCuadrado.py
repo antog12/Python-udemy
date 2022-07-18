7cf84eec2508ff8ca342a323ec2951fef0e07323
A.Angel <antog12@hotmail.com>
2022-07-18 14:01:32 +0200

    ejercicios varios

---
+++ areaCuadrado.py
@@ -0,0 +1,3 @@
+lado=int(input("Ingrese el lado del cuadrado: "))
+area= lado**2
+print("El area del cuadrado es: ",area)
---
+++ suma.py
@@ -0,0 +1,5 @@
+numero= int(input("Ingrese un numero: "))
+
+suma=numero*(numero+1)/2
+
+print("La suma de los numeros desde 1 hasta",numero,"es:",suma)
