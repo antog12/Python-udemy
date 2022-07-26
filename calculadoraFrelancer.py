2317c030a0c8567970d4a6934a57e161aa38bd5c
A.Angel <antog12@hotmail.com>
2022-07-26 23:06:41 +0200

    ejercicios varios

---
+++ calculadoraFrelancer.py
@@ -0,0 +1,12 @@
+# calculadora frelancer
+
+print("----------------------------------------------------")
+print("Bienvenido a la calculadora frelancer")
+print("----------------------------------------------------")
+dolaresPorHora=float(input("Ingrese los dolares por hora: "))
+pagoSemanal=40*dolaresPorHora
+pagoMensual=pagoSemanal*4
+print("----------------------------------------------------")
+print("El pago semanal es: ",pagoSemanal)
+print("El pago mensual es: ",pagoMensual)
+print("----------------------------------------------------")
